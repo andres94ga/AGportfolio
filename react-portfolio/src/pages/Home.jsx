8f69a99e2132c92fb8a96fa282af01ea201084b3
AndresGaona94 <andy_gaona09@hotmail.com>
2025-06-21 15:29:35 -0300

    Portfolio Finalizado

---
+++ react-portfolio/src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ThemeToggle } from "../components/ThemeToggle";
+import {} from "@/components/StarBackground";
+import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/NavBar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-hidden">
+      <ThemeToggle />
+      <StarBackground />
+      <Navbar />
+
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+        <Footer />
+      </main>
+    </div>
+  );
+};
