3b931d5a21d0a8535f1d3b11cb5f443bb4663b05
AndresGaona94 <andy_gaona09@hotmail.com>
2025-06-25 16:20:42 -0300

    Fixing Pdf file downloaded

--- react-portfolio/src/components/AboutSection.jsx
+++ react-portfolio/src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="../public/DenisCv.pdf"
+                href="/DenisCv.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
