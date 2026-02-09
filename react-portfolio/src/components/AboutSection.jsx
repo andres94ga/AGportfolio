ef56d5f49b2348fe6feba2ff6caf534ac18cb6f5
Andres94ga <andy_gaona09@hotmail.com>
2026-02-09 11:18:01 -0300

    cv changed

--- react-portfolio/src/components/AboutSection.jsx
+++ react-portfolio/src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/AndresGaonaCV.pdf"
+                href="/AndresGaona.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
